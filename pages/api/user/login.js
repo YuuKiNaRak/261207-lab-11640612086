f69f8af3cc1e9916ab40ff89d6aa8cbdd1f206a8
Yuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2022-10-18 21:52:03 +0700

    up

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -11,11 +11,33 @@ export default function login(req, res) {
     const users = readUsersDB();
 
     //find users with username, password
+    const user = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
+
+    if (!user)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid Username or Password" });
+
+    if (
+      typeof username !== "string" ||
+      username.length === 0 ||
+      typeof password !== "string" ||
+      password.length === 0
+    )
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username or password cannot be empty" });
 
     const secret = process.env.JWT_SECRET;
 
     //sign token
+    const token = jwt.sign({ username, isAdmin: user.isAdmin }, secret, {
+      expiresIn: "1800s",
+    });
 
     //return response
+    return res.json({ ok: true, username, isAdmin: user.isAdmin, token });
   }
 }
